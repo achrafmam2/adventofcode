d3d7265166cbac8e0101eb8c61cd94bf0d144299
achraf <achraf.mamdouh@yahoo.com>
2022-12-15 22:35:31 +0100

    Solve problem 15.

--- 2022/15/main.py
+++ 2022/15/main.py
@@ -8,10 +8,11 @@ import functools
 import operator
 import re
 
-
 FLAGS = flags.FLAGS
 _INPUT_FILE = flags.DEFINE_string('input', None, 'Path to the input file.')
-_VERBOSE = flags.DEFINE_bool('verbose', False, 'Include extra information during execution.')
+_VERBOSE = flags.DEFINE_bool('verbose', False,
+                             'Include extra information during execution.')
+
 
 class Point(collections.namedtuple('Point', 'x y')):
 
@@ -26,8 +27,10 @@ def dist(p0: Point, p1: Point) -> int:
   a, b = p1
   return abs(x - a) + abs(y - b)
 
+
 Sensors = Sequence[Point]
 Beacons = Sequence[Point]
+
 
 def parse(path: str) -> tuple[Sensors, Beacons]:
   with open(path) as f:
@@ -43,18 +46,19 @@ def parse(path: str) -> tuple[Sensors, Beacons]:
   #   >>> xs = [('1','a'),('2','b')]
   #   >>> list(zip(*xs))
   #   [('1', '2'), ('a', 'b')]
-  
+
   sensors, beacons = zip(*[parse_line(l) for l in lines])
   return (sensors, beacons)
 
 
-def nobeacon_interval(*,
-                      sensor: Point,
-                      beacon: Point,
-                      yline: int,
-                      ) -> I.Interval:
+def nobeacon_interval(
+    *,
+    sensor: Point,
+    beacon: Point,
+    yline: int,
+) -> I.Interval:
   """Returns an interval where no beacon can possibly exist."""
-  x, _ = sensor    
+  x, _ = sensor
   rad = dist(sensor, beacon) - dist(sensor, Point(x, yline))
   if rad < 0:
     return I.open(x, x)
@@ -63,6 +67,7 @@ def nobeacon_interval(*,
 
 def merge_intervals(intervals: Iterable[I.Interval]) -> I.Interval:
   return functools.reduce(operator.__or__, intervals)
+
 
 def interval_len(i: I.Interval) -> int:
   length = 0
@@ -74,6 +79,7 @@ def interval_len(i: I.Interval) -> int:
       width -= 1
     length += width
   return length
+
 
 def singleton(i: I.Interval) -> int:
   """Returns the only point in the input interval.
@@ -88,40 +94,45 @@ def singleton(i: I.Interval) -> int:
 
   assert l == r
   return l
-  
+
 
 def main(argv):
   sensors, beacons = parse(_INPUT_FILE.value)
 
   # Part 1.
   yline = 2000000
-  intervals = [nobeacon_interval(sensor=s, beacon=b, yline=yline)
-               for (s, b) in zip(sensors, beacons)]
+  intervals = [
+      nobeacon_interval(sensor=s, beacon=b, yline=yline)
+      for (s, b) in zip(sensors, beacons)
+  ]
   u = merge_intervals(intervals)
   for (x, y) in beacons:
     if y == yline:
-     u -= I.closed(x, x)
+      u -= I.closed(x, x)
 
   print(interval_len(u))
 
   # Part 2.
   n = 4000000
-  for y in range(n+1):
+  for y in range(n + 1):
     if _VERBOSE.value and (y % 10000 == 0):
       print('Iteration ', y)
 
-    intervals = [nobeacon_interval(sensor=s, beacon=b, yline=y)
-                 for (s, b) in zip(sensors, beacons)]
-    
+    intervals = [
+        nobeacon_interval(sensor=s, beacon=b, yline=y)
+        for (s, b) in zip(sensors, beacons)
+    ]
+
     u = merge_intervals(intervals)
-    u &= I.closed(0, n) # Clip to [0, n].
-    
+    u &= I.closed(0, n)  # Clip to [0, n].
+
     if u != I.closed(0, n):
       # Get the singleton point not covered by the interval u.
       u = ~u & I.closed(0, n)
       x = singleton(u)
       print(x * n + y)
       break
+
 
 if __name__ == '__main__':
   app.run(main)
